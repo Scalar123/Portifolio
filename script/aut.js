54831ffd5cc64138750f5dceed22d2eced0f3254
Scalar123 <scalar1122@gmail.com>
2024-02-23 15:36:21 -0300

    New port

--- script/aut.js
+++ script/aut.js
@@ -1,3 +1,15 @@
-function showlistheader(){
-    
+var count = 0 
+
+function menubar(){
+    var div_appear = document.getElementById("div_appear")
+    count = count + 1
+
+    if(count % 2 == 0){
+        div_appear.style.display = "inline"
+    }else{
+        div_appear.style.display = "none"
+    }
+
 }
+var btn = document.getElementById("btn");
+btn.addEventListener("click",menubar)
