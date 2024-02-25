9449198928d4aafd9a19ea76495fd26b8331c882
Scalar123 <scalar1122@gmail.com>
2024-02-24 21:50:11 -0300

    New updare main

--- script/aut.js
+++ script/aut.js
@@ -2,7 +2,9 @@ document.addEventListener("DOMContentLoaded",() => {
     new TypeIt("#animation_name", {
         speed: 300,
         loop: true,
-        }).type("＂Web Developer＂ }", {delay: 900}).move(-2, {delay: 900}).delete(14, {delay: 1000}).type("Kotlin Developer＂", {delay: 900}).delete(17, {deylay: 1000}).type("Angular＂", {delay: 900}).delete(8, {delay: 900}).type("Full-Stack＂", {delay: 900}).move(+2, {delay: 900})
+        }).type("＂Web Developer＂", {delay: 900}).delete(14, {delay: 1000}).type("Kotlin Developer＂", {delay: 900}).delete(17, {deylay: 1000}).type("Angular＂", {delay: 900}).delete(8, {delay: 900}).type("Full-Stack＂", {delay: 900})
     
     .go()
 })
+
+
