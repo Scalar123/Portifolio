88dbc19607ceb7d853bae041f0eaf2066cce54b5
Scalar123 <https://github.com/Scalar123/Portifolio.git>
2024-04-09 14:52:43 -0300

    Header Style almost finished

--- Version 2.0/script/menuReponsivo.js
+++ Version 2.0/script/menuReponsivo.js
@@ -1,13 +1,16 @@
 function resp(){
     var inmmm = document.getElementById("mmm");
     var listNav = document.getElementById("lista_nav");
+    var img_header = document.getElementById("img_header")
 
     var sizeWindow = window.screen.availWidth;
 
     if(sizeWindow <= 500){
-        listNav.className = "responsive_list_half"
+        listNav.className = "oculta"
+        img_header.className = "exibe"
     }else{
         listNav.className = "responsive_list_full"
+        img_header.className = "oculta"
     }
 
 }
